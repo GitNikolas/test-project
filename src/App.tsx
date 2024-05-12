176f6ee375a1573df9637dc738bd8534218e639f
LaptopNickolas <panteleew.kolya@gmail.com>
2024-05-12 13:35:45 +0900

    new file:   src/Map/Map.jsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Map } from './Map/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <Map></Map>
+
+    </div>
+  );
+}
+
+export default App;
